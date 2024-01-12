c28d50d00d7aec710024f1a3c6c3f8cda9839226
rajlulla <rajlulla@haylontech.com>
2024-01-12 13:51:30 -0500

    Initial Commit

---
+++ Transmission/TransmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransmissionApp.swift
+//  Transmission
+//
+//  Created by Raj Lulla on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TransmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
